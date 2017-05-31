dd8fd59802163fe2713e58600f0a429b7a9e84c4
sumitkute <sumit_kute@yahoo.com>
2017-05-31 15:09:52 +0530

    Data Project Added

---
+++ world.data/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace world.data
+{
+    public class Invoice
+    {
+        public Guid id { get; set; }
+        public int InvoiceNo { get; set; }
+        public string StockCode { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? InvoiceDate { get; set; }
+        public float? UnitPrice { get; set; }
+        public int? CustomerID { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ world.data/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace world.data
+{
+    public class People
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AadhaarNo { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public DateTime ?DOB { get; set; }
+    }
+}
